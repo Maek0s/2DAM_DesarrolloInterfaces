a6ef4f0229c3a9cad88d7d3947a3be086b1896c4
Maek0s <maek0szaho@gmail.com>
2024-10-02 21:48:53 +0200

    Primer proyecto en MAUI realizado.

---
+++ miquintoproyecto/miquintoproyecto/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace miquintoproyecto
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
