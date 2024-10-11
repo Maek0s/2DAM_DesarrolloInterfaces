f345c2178f46d942e2e06f503e974873bc306aff
Maek0s <145163254+Maek0s@users.noreply.github.com>
2024-10-11 19:33:55 +0200

    Actividad 2 subida a GitHub

---
+++ Actv2ConexionRemota/Apartado 1/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Examples
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Actv2ConexionRemota/Apartado 1/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Examples
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
